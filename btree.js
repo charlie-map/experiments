0c469106cf2f87c447bf638b4fdffacc9b98d9e0
Zach Minster <zach.minster@gmail.com>
2021-04-13 20:41:49 -0400

    leaf node cases handled, ostensibly

--- btree.js
+++ btree.js
@@ -85,6 +85,98 @@ function splitChild(node) {
 	return node;
 }
 
+function del(node, key) {
+	// (base) case: key not found
+	// TODO: search for and return
+	// (could probably handle this as we drop down if we are clever)
+
+	// case: root node contains key
+	// TODO: ??
+
+	// else: call helper in appropriate child
+	for (let i = 0; i <= node.keys.length; i++) {
+		if ((i < node.keys.length && key < node.keys[i]) || i == node.keys.length) {
+			del_helper(node.children[i], key, node, i);
+			break;
+		}
+	}
+}
+
+function del_helper(node, key, parent, keyPos) {
+	// Internal Node Cases
+	// TODO
+
+	// Leaf Node Cases
+	if (!node.children.length) {
+		let thresh = Math.floor(MAX_DEGREE / 2);
+		
+		// no matter what: remove the key
+		let removed, removedPos;
+		for (let i = 0; i < node.keys.length; i++) {
+			if (key == node.keys[i]) {
+				removed = node.keys.splice(i, 1);
+				removedPos = i;
+				break;
+			}
+		}
+		if (!removed) {	// (base case of nonexistent delete key)
+			return;
+		}
+
+		// case: no underflow
+		if (node.keys.length >= thresh) {
+			return;
+		}
+
+		console.log("keyPos", keyPos);
+
+		// case: underflow, left sibling exists and borrowable
+		if (keyPos-1 >= 0 && parent.children[keyPos-1].keys.length > thresh) {
+			// borrow from left sibling
+			let sibling = parent.children[keyPos-1];
+			let borrowKey = sibling.keys.splice(sibling.keys.length-1, 1);
+			// splay up borrowed key to parent
+			borrowKey = parent.keys.splice(keyPos-1, 1, ...borrowKey);
+			// insert into this node
+			node.keys.splice(0, 0, ...borrowKey);
+			// done
+			return;
+		}
+
+		// case: underflow, right sibling exists and borrowable
+		if (keyPos+1 < parent.keys.length && parent.children[keyPos+1].keys.length > thresh) {
+			let sibling = parent.children[keyPos+1];
+			let borrowKey = sibling.keys.splice(0, 1);
+			// splay up borrowed key to parent
+			borrowKey = parent.keys.splice(keyPos, 1, ...borrowKey);
+			// insert into this node
+			node.keys.push(...borrowKey);
+			// done
+			return;
+		}
+
+		// case: underflow, nonexistent / nonborrowable siblings
+		// we need to merge THIS node into sibling, shifting down a key from the parent node and removing child
+		let siblingPos;
+		siblingPos = keyPos - 1 >= 0 ? keyPos-1 : keyPos+1;
+		let sibling = parent.children[siblingPos];
+
+		// merge
+		sibling.keys.splice(siblingPos < keyPos ? node.keys.length-1: 0, node.keys.length, ...node.keys);
+
+		// sibling.children.splice(siblingPos < keyPos ? node.children.length-1 : 0, node.children.length, node.children); FUTURE MIDDLE CASE
+		node.keys = [];
+		node.children = [];
+
+		// correct parent node
+		console.log(siblingPos < keyPos ? sibling.keys.length : 0);
+		sibling.keys.splice(siblingPos < keyPos ? sibling.keys.length : 0, 0, ...parent.keys.splice(siblingPos < keyPos ? siblingPos : keyPos, 1));
+		parent.children.splice(keyPos, 1);
+
+		return;
+	}
+}
+
 /*let fake_test_node = node_factory();
 
 fake_test_node.keys = [3,5,8];
@@ -93,11 +185,12 @@ fake_test_node = splitChild(fake_test_node);
 
 console.log(JSON.stringify(fake_test_node));*/
 let root = node_factory();
-insert(root, 3);
-insert(root, 5);
-insert(root, 8);
-insert(root, 6);
+insert(root, 1);
+insert(root, 2);
 insert(root, 7);
+insert(root, 8);
 insert(root, 9);
-insert(root, 10);
+insert(root, 6);	// test middle cases
+console.log(JSON.stringify(root));
+del(root, 9);
 console.log(JSON.stringify(root));
